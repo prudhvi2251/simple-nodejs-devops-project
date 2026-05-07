ffdf009a20ba4ae83b174e2b86e64b23d5d22975
prudhvi <prudhvi.189559@gmail.com>
2026-05-07 15:13:06 +0530

    updated webhook

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('DevOps CI/CD Pipeline Working Successfully');
+  res.send('Webhook working');
 });
 
 app.listen(3000, () => {
